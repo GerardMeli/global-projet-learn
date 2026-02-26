07ace3a459ea01a9a32ea5923eb134c46bc4c499
reli237 <melingandjoug@gmail.com>
2026-02-26 16:52:28 +0100

    features/up to date

--- manage-users/src/main/kotlin/com/example/manage_users/service/interf/StatisticsService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.manage_users.service.interf
-
-import com.example.manage_users.dto.StatisticsDto
-import org.springframework.data.domain.Page
-import java.awt.print.Pageable
-
-interface StatisticsService {
-    fun getUserStatistics(): StatisticsDto.UserStatisticsResponse
-}
--- system-manager-file/src/main/kotlin/com/gateway/system_manager_file/dto/ApiResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.gateway.system_manager_file.dto
-
-class ApiResponse(
-    var success: Boolean,
-    var message: String
-)
